76bb9a9a4e552529ec703dd490f086ac2c09909e
drakeet <drakeet.me@gmail.com>
2015-11-22 15:56:35 +0800

    Small fix.

--- library/src/main/java/me/drakeet/materialdialog/MaterialDialog.java
+++ library/src/main/java/me/drakeet/materialdialog/MaterialDialog.java
@@ -4,7 +4,6 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.graphics.Color;
-import android.graphics.PixelFormat;
 import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.view.Gravity;
